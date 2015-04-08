855d9bdba088fbcaa0f71a3005cbbdc83fbd544f
johnptmcdonald <johnptmcdonald@gmail.com>
2015-04-07 18:44:41 -0700

    add chapter 2

---
+++ 02_chapter/loopingATriangle.js
@@ -0,0 +1,19 @@
+// Looping a triangle
+
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+var myString ="";
+
+for(var i = 0; i < 7; i++){
+	myString += "#";
+	console.log(myString);
+}
+
